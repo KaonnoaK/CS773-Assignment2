117a7eb0aae9e9cb075ec6380710dee10aa51d0b
Kaon <78657124+KaonnoaK@users.noreply.github.com>
2026-08-16 17:15:04 +0530

    Add files via upload

---
+++ pa2/obj/src/main.d
@@ -0,0 +1,30 @@
+obj/src/main.o: src/main.cc src/../inc/cache.h src/../inc/memory_class.h \
+ src/../inc/champsim.h src/../inc/block.h src/../inc/instruction.h \
+ src/../inc/set.h inc/ooo_cpu.h inc/cache.h inc/page_table_walker.h \
+ inc/uncore.h inc/champsim.h inc/dram_controller.h inc/memory_class.h
+
+src/../inc/cache.h:
+
+src/../inc/memory_class.h:
+
+src/../inc/champsim.h:
+
+src/../inc/block.h:
+
+src/../inc/instruction.h:
+
+src/../inc/set.h:
+
+inc/ooo_cpu.h:
+
+inc/cache.h:
+
+inc/page_table_walker.h:
+
+inc/uncore.h:
+
+inc/champsim.h:
+
+inc/dram_controller.h:
+
+inc/memory_class.h:
